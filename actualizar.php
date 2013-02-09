2d33b7274cc35ac0c428f14708511306b4d872aa
julian <julianrpo2711@hotmail.com>
2013-02-09 13:30:24 -0500

    verdatos

--- actualizar.php
+++ actualizar.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <html>
 <head><title>DatosCliente</title></head>
 <body>
@@ -27,3 +28,38 @@ Femenino<input type="radio" name = "sex" value ="female" ></p>
 </form>
 </body>
 </html>
+=======
+<?php
+
+$connect = mysql_connect("10.2.1.178", "tecnicas", "tecnicas");
+mysql_select_db("tecnicasavanzadas", $connect) or die("algo anda nmal");
+$id = $_get["id"]
+$query = 'select * from tbl_clients where id = '.$id.'';
+$result = mysql_query($query);
+$cliente = mysql_fetch_array($result);
+//echo "<td>" . $row['client'] . "</td>";
+//while ($row = mysql_fetch_array($result)){
+//echo $row['client'];
+?>
+<html>
+<body>
+
+<form action="construction.php" method="post">
+Nombre: <?php echo $cliente['client'] ?></br>
+</br>
+Tel&eacute;fono: <?php echo $cliente['phone'] ?> </br>
+</br>
+Direcci&oacute;n: <?php echo $cliente['adress'] ?></br>
+</br>
+Observaciones:<?php echo $cliente['observations'] ?></br>
+</br>
+Sexo:</br>
+</br>
+<input type="submit" value="Actualizar">
+</form>
+
+
+
+</body>
+</html>
+>>>>>>> 4b3a20d26bd886af3bee8a3caf3975e4c730594e
