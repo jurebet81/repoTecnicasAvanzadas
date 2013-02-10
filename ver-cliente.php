51325d8605a5d55be5cf6a6c369f9f25e3410c43
julian <julianrpo2711@hotmail.com>
2013-02-10 16:06:26 -0500

    ver-cliente FUNCIONANDO

--- ver-cliente.php
+++ ver-cliente.php
@@ -1,36 +1,24 @@
 <html>
 <head><title>DatosCliente</title></head>
 <body>
-<form name = "input" action="actualizar.php" method="POST">
+<form name = "input" aaction=<?php echo "actualizar.php?id=". $_GET["id"];?> method="POST">
 <?php
 $id=$_GET["id"];
-//$id = 2;
 $con = mysql_connect("10.2.1.178", "tecnicas", "tecnicas");
-//$con = mysql_connect("localhost", "root", "juli");
 mysql_select_db("tecnicasavanzadas", $con);
 $result = mysql_query("SELECT * FROM tbl_clients where id = ". $id);
-//while ($row = mysql_fetch_array($result)){
-	$row = mysql_fetch_array($result);
+$row = mysql_fetch_array($result);
+
 	echo "<p>Nombre: ". $row['client'] . " </p>";
 	echo "<p>Telefono: ". $row['phone'] . " </p>";
 	echo "<p>Direccion: ". $row['adress'] . " </p>";
 	echo "<p>Observaciones: ". $row['observations'] . " </p>";
 	echo "<p>Sexo: ". $row['sex'] . " </p>";
 	
-
-//}
 mysql_close($con);
 ?>
-<!--
-<p>Nombre:<input type= "text" name = "nombre"> </p>
-<p>Telefono:<input type= "text" name = "phone"> </p>
-<p>Direccion: <input type= "text" name = "address"> </p>
-<p>Observaciones: <input type = "textarea" name "obs"></p>
-<p>Sexo: Masculino<input type = "radio" name= "sex" value = "male">
-Femenino<input type="radio" name = "sex" value ="female" ></p>
--->
-<input type = "submit" value = "Actualizar Datos">
 
+<input type = "submit" value = "Editar informacion">
 </form>
 </body>
 </html>
