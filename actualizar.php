48e0a077f14109e0d875e5937c1bf99358164a46
julian <julianrpo2711@hotmail.com>
2013-02-10 17:28:33 -0500

    Ultimos detalles de actualizar y vercliente

--- actualizar.php
+++ actualizar.php
@@ -17,8 +17,7 @@
 	$query = $query . "', sex = '". $sex . "' WHERE id = ". $_GET["id"];
 	mysql_query($query);	
 	mysql_close($connect);
-	echo "Datos actualizados correctamente";
-	
+	echo "Datos actualizados correctamente";	
 	echo "<p><a href='index.html'>Salir</a></p>";
 	
 } else {
@@ -33,15 +32,25 @@
 	$obs = $client["observations"];
 	$sex = $client["sex"];
 	mysql_close($connect);
-
-	echo "<form action= actualizar.php?id=" . $_GET["id"] . "> method='POST'>";
+	
+	echo "<form action= actualizar.php?id=" . $_GET["id"] . " method='POST'>";
 	echo "<input type = 'hidden' name = 'id' value = '" . $_GET["id"] . "'>";
 	echo "<p>Nombre:<input type= 'text' name = 'name' value ='" . $name . "'></p>";
 	echo "<p>Telefono:<input type= 'text' name = 'phone' value ='" . $phone . "'></p>";
 	echo "<p>Direccion:<input type= 'text' name = 'address' value ='" . $adress . "'></p>";
 	echo "<p>Observaciones:<input type= 'text' name = 'obs' value ='" . $obs . "'> </p>";
-	echo "<p>Sexo: M <input type= 'radio' name = 'sex' value = 'M' " if ($sex == 'M'){echo 'checked'; } ">"; 
-	echo "F <input type= 'radio' name = 'sex' value = 'F' " if ($sex == 'F') { echo 'checked'; } "> </p>";
+	echo "<p>Sexo: M <input type= 'radio' name = 'sex' value = 'M' ";
+	if ( $sex == 'M')
+	{ 
+		echo "checked"; 
+	} 
+	echo " >";	
+	echo "F <input type= 'radio' name = 'sex' value = 'F' "; 
+	if ($sex == 'F') 
+	{
+		echo 'checked'; 
+	} 
+	echo " > </p>";
 	echo "<input type = 'submit' value = 'Actualizar'>";	
 	echo "</form>";
 }	
--- ver-cliente.php
+++ ver-cliente.php
@@ -1,10 +1,10 @@
 <html>
 <head><title>DatosCliente</title></head>
 <body>
-<form name = "input" aaction=<?php echo "actualizar.php?id=". $_GET["id"];?> method="POST">
+<form name = "input" action=<?php echo "actualizar.php?id=". $_GET["id"]; ?> method="POST">
 <?php
 $id=$_GET["id"];
-$con = mysql_connect("10.2.1.178", "tecnicas", "tecnicas");
+$con = mysql_connect("localhost", "tecnicas", "tecnicas");
 mysql_select_db("tecnicasavanzadas", $con);
 $result = mysql_query("SELECT * FROM tbl_clients where id = ". $id);
 $row = mysql_fetch_array($result);
